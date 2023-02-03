9e92390b5197e5d9d1d191791e8563b8ef7984db
BarkingAxe <ericknickberre@gmail.com>
2023-02-02 18:19:40 -0600

    Filled out about page and added spining cube

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -1,25 +1,59 @@
+import { faGithub, faHtml5, faJava, faJsSquare, faNode, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import AnimatedLetters from '../AnimatedLetters';
 
 
 const About = () => {
+    
+    const[letterClass,setLetterClass] = useState('text-animate'); 
+    
+    useEffect(()=>{
+        return setTimeout(()=>{
+          setLetterClass('text-animate-hover')
+        },3000)
+      },[])
+
+
+
     return(
+
         <div className="container about-page">
         <div className="text-zone">
                 <h1>
                     <AnimatedLetters
+                    letterClass={letterClass}
                     strArray={['A', 'B', 'O', 'U', 'T',' ','M','E']}
                     idx= {15}
                     />
                 </h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                     Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin.
-                     Sem et tortor consequat id porta nibh venenatis.
-                     Sed viverra tellus in hac habitasse. </p>
-                <p>Orci ac auctor augue mauris augue. Accumsan in nisl nisi scelerisque eu ultrices vitae. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Porta nibh venenatis cras sed felis. </p>
-                <p>Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Amet nulla facilisi morbi tempus iaculis urna. Quam adipiscing vitae proin sagittis. Enim ut sem viverra aliquet. Non tellus orci ac auctor augue mauris. Malesuada fames ac turpis egestas maecenas. Tincidunt praesent semper feugiat nibh sed pulvinar.</p>
-
+                <p>Hi am Nick. My current role is SQL Conversion Dev. I am familiar with the ETL process. </p>
+                <p>My current skills include SQL,Java, and I am currently learing full stack technologies </p>
+              
             </div>
+            <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faNode} color= '#87c7f7'/>
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faReact} color= '#87c7f7'/>
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faHtml5} color= '#87c7f7'/>
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faGithub} color= '#87c7f7'/>
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJava} color= '#87c7f7'/>
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faJsSquare} color= '#87c7f7'/>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -16,32 +16,20 @@ const Sidebar = () => (
     <img className = 'sub-logo'src= {LogoSubtitle} alt ="NICK"/>
 </Link>
 <nav>
-<NavLink 
-    exact = 'true' 
-    activeclassname = 'active' 
-    to="/">
-
-    <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-</NavLink>
-
-<NavLink 
-    exact = 'true' 
-    activeclassname = 'active'
-    className = "about-link"
-    to="/about">
-    
-    <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-</NavLink>
-
-<NavLink 
-    exact = 'true' 
-    activeclassname = 'active'
-    className = "contact-link" 
-    to="/contact">
-
-    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-</NavLink>
-</nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
 
 <ul>
     <li>
